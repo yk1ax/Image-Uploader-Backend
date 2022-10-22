9988f600f3d9a5197e4a4dde0ca63e721f7f45b9
yogesh-ti <yogesh.kumar@trilogy.com>
2022-10-23 01:22:32 +0530

    Modified for heroku

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const multer = require("multer");
 const fs = require("fs");
 
 const app = express();
+const port = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+
+    const allowedOrigins = ['http://localhost:3000', 'https://yk-image-uploader.herokuapp.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    return next();
+});
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         const path = "./uploads"
@@ -14,20 +27,13 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
-
 const upload = multer({ storage: storage }).single("image");
-const port = process.env.PORT || 3001;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.send("Response of GET request");
 });
 
 app.post('/upload', (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-
     upload(req, res, (err) => {
         if (err instanceof multer.MulterError) {
             console.log("A Multer error occurred when uploading.");
@@ -35,6 +41,7 @@ app.post('/upload', (req, res) => {
             console.log("An unknown error occurred when uploading: " + err);
         }
         
+        console.log("Image upload received");
         res.send("POST request received")
     });
 });
